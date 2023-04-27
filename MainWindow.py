ee10c0b7393c00f71356dece6facf5f601234741
Elion Marku <71005986+ElionMarku00@users.noreply.github.com>
2023-04-27 09:09:54 +0200

    before pulling borna's work

---
+++ MainWindow.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication, QWidget,QPushButton,QMainWindow
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
