4e05543b09406aacc0b6f35baab155c87f59d2ef
Borna Talebi <49802660+bornatalebi@users.noreply.github.com>
2023-04-27 17:12:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,28 @@ dpg.create_context()
 temp = []
 numbers = []
 
+# add a font registry
+with dpg.font_registry():
+    # first argument ids the path to the .ttf or .otf file
+    default_font = dpg.add_font("times.ttf", 20)
+
 
 def result_callback(sender, data):
     current_value = dpg.get_value("Display")
     dpg.set_value("Display", str(eval(current_value)))
+    # add the last number that is inside the temp list, since we do not run the add callback for it
+    # last_num = int(''.join(str(item) for item in temp))
+    # numbers.append(last_num)
+    # temp.clear()
+    # ###
+    # # calculate the sum of all numbers in the "numbers" list
+    # result = sum([num for num in numbers])
+    # # print the numbers list to the console for debugging purposes
+    # print(numbers)
+    # # set the value of the "Display" item to the calculated result
+    # dpg.set_value("Display", str(result))
+    # # clear the "numbers" list for the next calculation
+    # numbers.clear()
 
 
 def add_callback(sender, data):
@@ -29,6 +47,7 @@ def sub_callback(sender, data):
 
 
 def num_callback(sender, data):
+
     # get the digit of the button that was pressed
     digit = dpg.get_item_label(sender)
     # temp.append(digit)
@@ -37,13 +56,28 @@ def num_callback(sender, data):
     # update the "Display" item to show the new value
     dpg.set_value("Display", str(current_value) + str(digit))
 
-# utility to clear the screen
+
 def clear_callback():
-    dpg.set_value("Display",'')
+    dpg.set_value("Display", '')
+def backspace_callback():
+    # get the current value of the "Display" item
+    current_value = dpg.get_value("Display")
+    dpg.set_value("Display", str(current_value)[:-1])
+
 # create a window with a table to hold the buttons
-with dpg.window(label="Tutorial"):
+dpg.bind_font(default_font)
+with dpg.window(label="Tutorial", tag="Primary Window"):
     dpg.add_separator()
     dpg.add_text("", tag="Display")
+    dpg.add_separator()
+    Button('<--', backspace_callback)
+    dpg.add_separator()
+    # adding trigonometry
+    with dpg.group(horizontal=True):
+        Button("sin", num_callback, button_type='trigo')
+        Button("cos", num_callback, button_type='trigo')
+        Button("tan", num_callback, button_type='trigo')
+        #TODO based on the button_type chnage the default value. for trigono we need to put the 0
     dpg.add_separator()
     with dpg.table(tag="Table", header_row=False):
         # add three columns to the table
@@ -59,24 +93,23 @@ with dpg.window(label="Tutorial"):
                         Button("+", add_callback)
                     elif i * 3 + j + 1 == 12:
                         # add a button for subtraction
-                        # dpg.add_button(label="-", width=50, height=50, callback=sub_callback)
                         Button("-", sub_callback)
                     elif i * 3 + j + 1 == 11:
                         # add a button for 0
-                        # dpg.add_button(label="0", width=50, height=50, callback=num_callback)
                         Button("0", num_callback)
                     else:
                         # add a button for the number
-                        # dpg.add_button(label=str(i * 3 + j + 1), width=50, height=50, callback=num_callback)
                         Button(str(i * 3 + j + 1), num_callback)
         with dpg.table_row():
             Button('(', num_callback)
             Button(')', num_callback)
             Button('C', clear_callback)
     dpg.add_button(label="=", width=165, height=50, callback=result_callback)
+
 dpg.create_viewport(title='Custom Title', width=200, height=500)
 dpg.setup_dearpygui()
 dpg.show_viewport()
+# dpg.set_primary_window("Primary Window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
 
