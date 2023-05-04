eb47f334aded2bce2a5484ef8cb4a104ec87c5e0
Borna Talebi <49802660+bornatalebi@users.noreply.github.com>
2023-05-04 16:31:38 +0200

    updated main.py
    1- used the same callback method for all buttons(check if there is any error)
    2- refactor the button assignment to use list comprehension

--- main.py
+++ main.py
@@ -1,32 +1,4 @@
 from button import Button
-from callbacks import result_callback, add_callback, sub_callback, num_callback, clear_callback, backspace_callback,type_text,factorial_callback
-import dearpygui.dearpygui as dpg
-from History import History
-
-dpg.create_context()
-
-temp = []
-numbers = []
-hist = History()
-
-# add a font registry
-# with dpg.font_registry():
-    # first argument ids the path to the .ttf or .otf file
-    # default_font = dpg.add_font("times.ttf", 20)
-
-def on_selection(sender, data):
-    selected_item = hist[data[0]]
-    print(f"Selected item: {selected_item}")
-
-def toggleHistory(sender, app_data, user_data):
-    
-    x, y = dpg.get_item_pos(primaryWindow)
-    width, height = dpg.get_item_rect_size(primaryWindow)
-    
-    dpg.set_item_pos(histwindow, [ x+width, y])
-    if not dpg.is_item_shown('historyWindow'):
-        dpg.show_item('historyWindow')
-    else: dpg.hide_item('historyWindow')
 import dearpygui.dearpygui as dpg
 
 from callbacks import num_callback, type_text, result_callback
@@ -64,29 +36,8 @@ with dpg.window(label="Tutorial", tag="Primary Window") as primaryWindow:
         for i in range(0, len(button_list)):
             # create a row and read one of the nested list items
             with dpg.table_row():
-                for j in range(0, 3):
-                    if i * 3 + j + 1 == 10:
-                        Button("+", add_callback)
-                    elif i * 3 + j + 1 == 12:
-                        Button("-", sub_callback)
-                    elif i * 3 + j + 1 == 11:
-                        Button("0", num_callback)
-                    else:
-                        Button(str(i * 3 + j + 1), num_callback)
-        with dpg.table_row():
-            Button('(', num_callback)
-            Button(')', num_callback)
-            Button('C', clear_callback)
-        with dpg.table_row():
-            Button('!', factorial_callback)
-            
-    dpg.add_button(label="=", width=165, height=50, callback=result_callback, user_data=hist)
-
-with dpg.window(label="history", tag="historyWindow",show=False) as histwindow:
-#    if len(hist._data) > 0:
-    dpg.add_listbox(label="Operations",items=[str(x) for x in hist._data ], tag="List")
-
-
+                [Button(x, num_callback) for x in button_list[i]]
+    Button('=', result_callback)
 with dpg.handler_registry():
     dpg.add_key_press_handler(callback=type_text)
 
