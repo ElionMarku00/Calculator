d7698641b1926719f458a8bbd448d42d29298f17
Borna Talebi <49802660+bornatalebi@users.noreply.github.com>
2023-05-04 16:31:38 +0200

    updated main.py
    1- used the same callback method for all buttons(check if there is any error)
    2- refactor the button assignment to use list comprehension

--- main.py
+++ main.py
@@ -1,61 +1,46 @@
-import dearpygui.dearpygui as dpg
 from button import Button
+import dearpygui.dearpygui as dpg
 
-from callbacks import result_callback, add_callback, sub_callback, num_callback, clear_callback, backspace_callback,type_text
+from callbacks import num_callback, type_text, result_callback
 
-dpg.create_context()
-
-temp = []
-numbers = []
+# The list of main buttons to use
+button_list = [['(', ')', 'c', '<--'], [7, 8, 9, '%'], [4, 5, 6, "*"], [1, 2, 3, "-"], ["+/-", 0, '.', '+']]
 
 # add a font registry
 # with dpg.font_registry():
-    # first argument ids the path to the .ttf or .otf file
-    # default_font = dpg.add_font("times.ttf", 20)
+# first argument ids the path to the .ttf or .otf file
+# default_font = dpg.add_font("times.ttf", 20)
 
-
+dpg.create_context()
 
 # dpg.bind_font(default_font)
 with dpg.window(label="Tutorial", tag="Primary Window"):
     dpg.add_separator()
     dpg.add_text("", tag="Display")
     dpg.add_separator()
-    Button('<--', backspace_callback)
+
     dpg.add_separator()
     with dpg.group(horizontal=True):
         Button("sin", num_callback, button_type='trigo')
         Button("cos", num_callback, button_type='trigo')
         Button("tan", num_callback, button_type='trigo')
-        #TODO based on the button_type chnage the default value. for trigono we need to put the 0
+        # TODO based on the button_type chnage the default value. for trigono we need to put the 0
     dpg.add_separator()
     with dpg.table(tag="Table", header_row=False):
-        # add three columns to the table
+        # add columns to the table
         dpg.add_table_column()
         dpg.add_table_column()
         dpg.add_table_column()
-        for i in range(0, 4):
+        dpg.add_table_column()
+        for i in range(0, len(button_list)):
+            # create a row and read one of the nested list items
             with dpg.table_row():
-                for j in range(0, 3):
-                    if i * 3 + j + 1 == 10:
-                        Button("+", add_callback)
-                    elif i * 3 + j + 1 == 12:
-                        Button("-", sub_callback)
-                    elif i * 3 + j + 1 == 11:
-                        Button("0", num_callback)
-                    else:
-                        Button(str(i * 3 + j + 1), num_callback)
-        with dpg.table_row():
-            Button('(', num_callback)
-            Button(')', num_callback)
-            Button('C', clear_callback)
-    dpg.add_button(label="=", width=165, height=50, callback=result_callback)
-
-
+                [Button(x, num_callback) for x in button_list[i]]
+    Button('=', result_callback)
 with dpg.handler_registry():
-    dpg.add_key_press_handler( callback=type_text)
+    dpg.add_key_press_handler(callback=type_text)
 
-
-dpg.create_viewport(title='Custom Title', width=200, height=500)
+dpg.create_viewport(title='Custom Title', width=200, height=800)
 dpg.setup_dearpygui()
 dpg.show_viewport()
 # dpg.set_primary_window("Primary Window", True)
