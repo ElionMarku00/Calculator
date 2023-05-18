e887c506bc10003a634ef76398a0c326f17d6bff
Borna Talebi <49802660+bornatalebi@users.noreply.github.com>
2023-05-18 22:01:47 +0200

    square root, square and absolute value functions added
    - the name of trigo_callback was changed to the advance_op_callback to be more general
    - added more comments
    - fix small bugs

--- callbacks.py
+++ callbacks.py
@@ -2,17 +2,24 @@ import dearpygui.dearpygui as dpg
 import math
 from math import sin, cos, tan
 
+# This file contains all the callback method used when clicking a button
+
 # checks if the last button that was clicked was the result button
 result_calculated = False
 
 
+# calculates the value showed on the display when the "=" button is clicked
 def result_callback(sender, data, user_data):
+    # defines new functions that are not supported by default
+    local_vars = {"abslt": lambda x: x if x >= 0 else -x, "pot": lambda x: x ** 2, "sqrt": lambda x: x ** 0.5}
+    global_vars = {}
     current_value = dpg.get_value("Display")
     try:
-        res = str(eval(current_value))
+        res = str(eval(current_value, global_vars, local_vars))
         dpg.set_value("Display", res)
     except ZeroDivisionError:  # catching divided by zero
         dpg.set_value("Display", 'Cannot divide by zero')
+    # change the boolean
     global result_calculated
     result_calculated = True
     # store operation and result in history
@@ -36,41 +43,54 @@ def toggleHistory(sender, app_data, user_data):
         dpg.hide_item('historyWindow')
 
 
-def trigo_callback(sender, data):
+# used in trigonometry, square root and power of two
+def advance_op_callback(sender, data):
+    global result_calculated
+    result_calculated = False
     trigonometry_function = dpg.get_item_label(sender)
     current_display_value = dpg.get_value("Display")
     dpg.set_value("Display", str(trigonometry_function) + '(' + str(current_display_value) + ')')
 
 
+# used for numpad
 def num_callback(sender, data):
     current_value = dpg.get_value("Display")
     global result_calculated
     # if the result button was clicked or the first number on the screen is zero, clear the screen
     if result_calculated or current_value[0] == '0':
         dpg.set_value("Display", '')
+        # now that the result button is not the last button clicked, change the bool
         result_calculated = False
     digit = dpg.get_item_label(sender)
     current_value = dpg.get_value("Display")
     dpg.set_value("Display", str(current_value) + str(digit))
 
 
+# used for basic mathematical operations
 def operator_callback(sender, data):
+    global result_calculated
+    result_calculated = False
     digit = dpg.get_item_label(sender)
     current_value = dpg.get_value("Display")
-    # if the last item on display is an operator, replace it with the new one
+    # if the last item on display is an operator, replace it with the new one. used to prevent adding extra operator
     if current_value[-1] in ['+', '-', '*', '/']:
         dpg.set_value("Display", str(current_value[:-1]) + str(digit))
     else:
         dpg.set_value("Display", str(current_value) + str(digit))
 
 
+# reset the diplay to default value
 def clear_callback():
-    dpg.set_value("Display", '')
+    dpg.set_value("Display", '0')
 
 
+# removes one character from the display
 def backspace_callback():
     current_value = dpg.get_value("Display")
-    dpg.set_value("Display", str(current_value)[:-1])
+    if len(current_value) == 1:
+        dpg.set_value("Display", '0')
+    else:
+        dpg.set_value("Display", str(current_value)[:-1])
 
 
 def type_text(sender, app_data):
