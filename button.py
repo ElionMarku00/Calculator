8cdf4f741c061ba5f1bbaedb1ef4d8a1ceb7f68b
Borna Talebi <49802660+bornatalebi@users.noreply.github.com>
2023-05-18 21:59:14 +0200

    Update button.py

--- button.py
+++ button.py
@@ -1,15 +1,16 @@
 import dearpygui.dearpygui as dpg
 
 from callbacks import num_callback
+# This file contains the button class
 
 class Button:
-    def __init__(self, value, user_data=None, callback_function=num_callback, width=50, height=50, button_type='default'):
+    def __init__(self, value, user_data=None, callback_function=num_callback, width=50, height=50,
+                 button_type='default'):
         dpg.add_button(
-            label=str(value), 
-                        width=width,
-                        height=height,
-                        callback=callback_function, #the default callback funtion
-                        # user_data=button_type,
-                        user_data=user_data,
-                        tag=value)
-        
+            label=str(value),
+            width=width,
+            height=height,
+            callback=callback_function,  # the default callback funtion
+            # user_data=button_type,
+            user_data=user_data,
+            tag=value)
